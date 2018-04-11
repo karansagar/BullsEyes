2033682fe4293ee75a2a0777ebb94a0d42204e9a
karan <karan.sagar1028@gmail.com>
2018-04-11 06:23:00 +0530

    added About page, and another page "about Developer"

---
+++ BullsEyes/AboutMeViewController.swift
@@ -0,0 +1,31 @@
+//
+//  AboutMeViewController.swift
+//  BullsEyes
+//
+//  Created by Karan Sagar on 11/04/18.
+//  Copyright © 2018 Karan Sagar. All rights reserved.
+//
+
+import UIKit
+
+class AboutMeViewController: UIViewController {
+    
+    @IBAction func backToGame() {
+        dismiss(animated: true, completion: nil)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
---
+++ BullsEyes/AboutViewController.swift
@@ -0,0 +1,27 @@
+//
+//  AboutViewController.swift
+//  BullsEyes
+//
+//  Created by Karan Sagar on 11/04/18.
+//  Copyright © 2018 Karan Sagar. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+}
